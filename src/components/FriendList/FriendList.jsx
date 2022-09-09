2b3b1e7c8fa5a05b22d530272dac0bc67f367d6a
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-09-09 22:05:36 +0300

    friends

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
+          />
+        );
+      })}
+    </ul>
+  );
+};
